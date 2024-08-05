c2f5d9b4fbd89f4b22ecf693a72e2f2ef102c1cd
Vibhorpersonals <vibhorsharma969@gmail.com>
2024-08-05 21:05:11 +0530

    done do this

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/portfolio3.0/", // Replace 'your-repo-name' with your actual repo name
+  base: "/portfolio/", // Replace 'your-repo-name' with your actual repo name
   plugins: [react()],
 });
